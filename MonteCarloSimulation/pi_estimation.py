d660f473490aef07cdbdfb942ee01c4851835f06
zhiyingTan0 <50588149+zhiyingTan0@users.noreply.github.com>
2025-04-25 22:08:43 -0400

    pi estimate

---
+++ MonteCarloSimulation/pi_estimation.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+N = 10000
+A = np.random.rand(N*2).reshape((N,2)) # N points (x,y)
+pts_in_circle = np.sum(A[:, 0]**2 + A[:, 1]**2 <= 1)
+print("Estimated pi is:", 4*pts_in_circle/N)
+
